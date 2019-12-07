a6be9df22b0d287364b6d82b544886df9658f861
chrispan68 <chrispan68@gmail.com>
2019-12-07 16:20:29 -0500

    added eve's data preprocessing

--- src/model.py
+++ src/model.py
@@ -6,6 +6,7 @@ import torch.nn.utils
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
+
 class RNN_Model(nn.Module):
     """Basic RNN LSTM classifier
 
@@ -13,10 +14,12 @@ class RNN_Model(nn.Module):
     @param hidden_size (int): Size of hidden vector
     @param vocab (List[str]): list of wor
     """
-    def __init__(self , embed_size , hidden_size , vocab):
+
+    def __init__(self, embed_size, hidden_size, vocab):
         self.hidden_size = hidden_size
         self.embed_size = embed_size
-        self.model_embeddings = nn.Embedding(len(vocab) , embed_size , padding_idx=0)
+        self.model_embeddings = nn.Embedding(
+            len(vocab), embed_size, padding_idx=0)
 
-        #default values
+        # default values
         self.
